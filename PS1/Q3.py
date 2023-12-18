ca1a62b4247f329d65e06239775b857a85ecb15f
Livin Joseph J <livinjoseph66@gmail.com>
2023-12-18 20:22:48 +0530

    Update Q3.py

--- PS1/Q3.py
+++ PS1/Q3.py
@@ -5,42 +5,6 @@ Created on Mon Dec 18 00:56:10 2023
 @author: livin
 """
 
-def ConvertToRREF(a):
-    n = len(a)
-    i = 0
-    j = 0
-    k = 0
-    c = 0
- 
-    # Performing elementary operations
-    for i in range(n):
-        if (a[i][i] == 0):
- 
-            c = 1
-            while ((i + c) < n and a[i + c][i] == 0):
-                c += 1
-            if ((i + c) == n):
-                 break
- 
-            j = i
-            for k in range(1 + n):
- 
-                temp = a[j][k]
-                a[j][k] = a[j+c][k]
-                a[j+c][k] = temp
- 
-        for j in range(n):
- 
-            # Excluding all i == j
-            if (i != j):
-                # Converting Matrix to reduced row
-                # echelon form(diagonal matrix)
-                p = a[j][i] / a[i][i]
- 
-                k = 0
-                for k in range(n):
-                    a[j][k] = a[j][k] - (a[i][k]) * p
-
 import sympy as sp
 import numpy as np
 
@@ -67,6 +31,3 @@ print("Original matrix")
 print(mat)
 print("RREF")
 print(RREF)
-
-ConvertToRREF(mat)
-print(mat)
