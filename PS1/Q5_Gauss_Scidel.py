85aab69056b1706d3a8de21ab43d94c493087b28
Livin Joseph J <livinjoseph66@gmail.com>
2023-12-25 20:05:11 +0530

    Update Q5_Gauss_Scidel.py

--- PS1/Q5_Gauss_Scidel.py
+++ PS1/Q5_Gauss_Scidel.py
@@ -5,26 +5,92 @@ Created on Mon Dec 18 22:33:53 2023
 @author: livin
 """
 
-import numpy as np
+def diagdom(coeff_matrix, const_matrix):
+    import numpy as np
+    import DiagonallyDominant as dd
+    eqn = coeff_matrix.tolist()
+    const = const_matrix.tolist()
+    
+    for i in range(len(eqn)):
+        for j in range(len(eqn)):
+            max1 = max(eqn[j])
+            index = eqn[j].index(max1)
+            eqn[index], eqn[j] = eqn[j], eqn[index]
+            const[index], const[j] = const[j], const[index]
+    
+    new_coeff_matrix = np.array(eqn)
+    new_const_matrix = np.array(const)
+    new_aug_matrix = np.concatenate((new_coeff_matrix, new_const_matrix), axis=1)
 
-mat = []
+    flag = dd.strictlyDiagonallyDominant(new_coeff_matrix)
+    if flag == True:
+        return tuple([True, new_aug_matrix])
+    else:
+        return tuple([False])
+    
+def gauss_scidel(aug_matrix, var):
+    for i in range(len(aug_matrix)):
+        temp = aug_matrix[i][-1]
+        for j in range(len(aug_matrix[0])-1):
+            if i == j:
+                continue
+            temp = temp - aug_matrix[i][j] * var[j]
+        temp = temp / aug_matrix[i][i]
+        var[i] = temp
+    return var
 
-n = int(input("Enter number of variables/equations: "))
+def main():
+    import numpy as np
 
-coeff_matrix = []
-const_matrix = []
+    n = int(input("Enter number of variables/equations: "))
 
-print("Enter the elements of the coefficient matrix (row-wise)")
-for i in range(n):
-    arr = []
-    for j in range(n):
-        arr.append(int(input()))
-    coeff_matrix.append(arr)
+    coeff_matrix = []
+    const_matrix = []
 
-coeff_matrix = np.array(coeff_matrix)
+    print("Enter the elements of the coefficient matrix (row-wise)")
+    for i in range(n):
+        arr = []
+        for j in range(n):
+            arr.append(int(input()))
+        coeff_matrix.append(arr)
 
-print("Enter the elements of the constant matrix (row-wise)")
-for i in range(n):
-    const_matrix.append([int(input())])
+    coeff_matrix = np.array(coeff_matrix)
 
-const_matrix = np.array(const_matrix)
+    print("Enter the elements of the constant matrix (row-wise)")
+    for i in range(n):
+        const_matrix.append([int(input())])
+
+    const_matrix = np.array(const_matrix)
+
+    temp = diagdom(coeff_matrix, const_matrix)
+    
+    if temp[0] == False:
+        print("Given equations cannot be converted into a strictly diagonally dominant matrix")
+        import sys
+        sys.exit()
+
+    aug_matrix = temp[1]
+    print("Given equations are converted into a strictly diagonally dominant matrix")
+    print("Augumented matrix")
+    print(aug_matrix)
+    
+    print("Enter initial values of variables")
+    var = []
+    for i in range(len(aug_matrix[0])-1):
+        var.append(int(input()))
+
+    import copy
+    temp_var = []
+    count = 1
+    while temp_var != var:
+        temp_var = copy.deepcopy(var)
+        var = gauss_scidel(aug_matrix, var)
+        print("Iteration ", count)
+        count += 1
+        print(var)
+    
+    print("Solution")
+    print(var)
+
+if __name__ == '__main__':
+    main()
