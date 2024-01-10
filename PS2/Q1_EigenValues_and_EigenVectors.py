a888a4572922b3a9b360d7d9e22b43e26e25ee2d
Livin Joseph J <livinjoseph66@gmail.com>
2024-01-10 23:15:44 +0530

    Update Q1_EigenValues_and_EigenVectors.py

--- PS2/Q1_EigenValues_and_EigenVectors.py
+++ PS2/Q1_EigenValues_and_EigenVectors.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan  1 21:35:50 2024
+Created on Mon Jan 1 21:35:50 2024
 
 @author: livin
 """
 
 def main():
     import numpy as np
+    import sympy as sp
 
     mat = []
 
@@ -17,26 +18,32 @@ def main():
     for i in range(n):
         arr = []
         for j in range(n):
-            arr.append(float(input()))
+            arr.append(int(input()))
         mat.append(arr)
 
     mat = np.array(mat)
+    mat = sp.Matrix(mat)
     print(mat)
 
+    """
     import sys
     sys.path.append("C:/Users/livin/GitHub/Scientific-Computing-Lab/PS1")
     import Q3_Matrix_to_RREF as rref
-    
-    charPolyCoeffs = [1, -sum([mat[x][x] for x in range(len(mat))])]
-    a11 = mat[1][1] * mat[2][2] - mat[2][1] * mat[1][2]
-    a22 = mat[0][0] * mat[2][2] - mat[2][0] * mat[0][0]
-    a33 = mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1]
-    charPolyCoeffs.append(a11 + a22 + a33)
-    charPolyCoeffs.append(np.linalg.det(mat))
-    
-    eigenvalues = np.roots(charPolyCoeffs)
-    print("Eigen values")
-    print(eigenvalues)
+    """
+
+    lam = sp.Symbol('lambda')
+    newmat = mat - lam * sp.eye(n)
+    char_eq = newmat.det()
+    print("Characteristic equation: ",char_eq)
+    ev = sp.solveset(char_eq, lam)
+    ev = [round(i, 2) for i in ev]
+    print("Eigenvalues: ", ev)
+
+    for i in ev:
+        tempmat = newmat.subs(lam, i)
+        tempmat = tempmat.col_insert(n, sp.Matrix([0] * n))
+        evec = sp.linsolve(tempmat)
+        print("Eigenvectors corresponding to eigenvalue ", i, ": ", evec)
 
 if __name__ == '__main__':
     main()
